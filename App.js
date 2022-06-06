dd5f9e1167ccff2c37f16590e3d288251e2c5f1b
nikhil149 <nikhil123rachawar@gmail.com>
2022-06-06 10:52:02 +0530

    Added flex styles

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder="Your Goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>Your Goal list...</Text>
       </View>
     </View>
@@ -16,17 +16,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
     display: "flex",
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 2,
     borderColor: "#cccccc",
-    width: "80%",
-    marginRight: 2,
+    width: "70%",
+    marginRight: 5,
+    padding: 5,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
